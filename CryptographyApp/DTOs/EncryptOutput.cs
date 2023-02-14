22ed7f453521e6c9195d21efe650f6802a1f6dc4
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-02-14 09:45:31 -0300

    default

--- CryptographyApp/DTOs/EncryptOutput.cs
+++ CryptographyApp/DTOs/EncryptOutput.cs
@@ -2,9 +2,9 @@
 {
     public class EncryptOutput
     {
-        public string CreditCardToken { get; set; }
+        public string CreditCardToken { get; set; } = default!;
         public long Id { get; set; }
-        public string UserDocument { get; set; }
+        public string UserDocument { get; set; } = default!;
         public long Value { get; set; }
     }
 }
